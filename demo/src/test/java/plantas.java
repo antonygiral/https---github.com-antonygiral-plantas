b4acab0abf142227faab4e213b829fe09f498dff
antonygiral <antonygiral@gmail.com>
2023-10-04 06:10:59 +0200

    dfsd

---
+++ demo/src/test/java/plantas.java
@@ -0,0 +1,17 @@
+public class Planta {
+    String nombrecomun;
+    String nombrecientifico;
+    String tipo;
+    String requerimientosluz;
+    String requerimientosagua;
+
+    public Planta(String nombrecomun, String nombrecientifico, String tipo, String requerimientosluz, String requerimientosagua) {
+        this.nombreComun = nombreComun;
+        this.nombreCientifico = nombreCientifico;
+        this.tipo = tipo;
+        this.requerimientosLuz = requerimientosLuz;
+        this.requerimientosAgua = requerimientosAgua;
+    }
+    
+    
+}
