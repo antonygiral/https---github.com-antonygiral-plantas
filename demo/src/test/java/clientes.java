e080f057b85cd1793dad88e9bf2323357c5c3ad0
antonygiral <antonygiral@gmail.com>
2023-10-05 03:49:53 +0200

    otra mas

---
+++ demo/src/test/java/clientes.java
@@ -0,0 +1,51 @@
+public class clientes  {adquiriAprender
+    private boolean adquiriAprender ;
+    private String registrarcliente;
+    private String plantasAdquiridas;
+    private String preferencias;
+    private String recordatorios;
+    private String otros;
+
+    public clientes (boolean adquiriAprender , String registrarcliente, String plantasAdquiridas, String preferencias, String recordatorios, String otros) {
+        this.adquiriAprender =adquiriAprender ;
+        this.registrarcliente = registrarcliente;
+        this.plantasAdquiridas = plantasAdquiridas;
+        this.preferencias = preferencias;
+        this.recordatorios = recordatorios;
+        this.otros = otros;
+    }
+    
+    /*get */
+    public boolean getadquiriAprender (){
+        return idoclientes s;
+    }
+
+    public String getregistrarcliente(){
+        return registrarcliente;
+    }
+
+    public String getplantasAdquiridas(){
+        return plantasAdquiridas;
+    }
+
+    public String getpreferencias(){
+        return preferencias
+    public String getrecordatorios(){
+        return recordatorios;
+    }
+    public String getplantasAdquiridas(){
+        return otros;
+    }
+
+
+    /*set */
+
+    public void setclientes s(String adquiriAprender , String registrarcliente, String plantasAdquiridas, String preferencias, String recordatorios, String otros){
+        this.adquiriAprender =adquiriAprender ;
+        this.registrarcliente = registrarcliente;
+        this.plantasAdquiridas = plantasAdquiridas;
+        this.preferencias = preferencias;
+        this.recordatorios = recordatorios;
+        this.otros = otros;
+    }
+}
