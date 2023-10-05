59c319c48933c9365a3b49865e863920ab13ecff
antonygiral <antonygiral@gmail.com>
2023-10-05 03:35:35 +0200

    dia miercoles

--- demo/src/test/java/plantas.java
+++ demo/src/test/java/plantas.java
@@ -1,12 +1,12 @@
 public class Planta {
-    private String idplanta;
+    private int idplanta;
     private String nombrecomun;
     private String nombrecientifico;
     private String tipo;
     private String requerimientosluz;
     private String requerimientosagua;
 
-    public Planta(String idplanta, String nombrecomun, String nombrecientifico, String tipo, String requerimientosluz, String requerimientosagua) {
+    public Planta(int idplanta, String nombrecomun, String nombrecientifico, String tipo, String requerimientosluz, String requerimientosagua) {
         this.idplanta=idplanta;
         this.nombrecomun = nombrecomun;
         this.nombrecientifico = nombrecientifico;
@@ -15,5 +15,37 @@ public class Planta {
         this.requerimientosagua = requerimientosagua;
     }
     
-    
+    /*get */
+    public int getidplanta(){
+        return idoplantas;
+    }
+
+    public String getnombrecomun(){
+        return nombrecomun;
+    }
+
+    public String getnombrecientifico(){
+        return nombrecientifico;
+    }
+
+    public String gettipo(){
+        return tipo
+    public String getrequerimientosluz(){
+        return requerimientosluz;
+    }
+    public String getnombrecientifico(){
+        return requerimientosagua;
+    }
+
+
+    /*set */
+
+    public void setplantas(int idplanta, String nombrecomun, String nombrecientifico, String tipo, String requerimientosluz, String requerimientosagua){
+        this.idplanta=idplanta;
+        this.nombrecomun = nombrecomun;
+        this.nombrecientifico = nombrecientifico;
+        this.tipo = tipo;
+        this.requerimientosluz = requerimientosluz;
+        this.requerimientosagua = requerimientosagua;
+    }
 }
